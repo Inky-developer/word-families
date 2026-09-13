3f967fa387ac0b55f4912d992c5dea674d58cc21
Inky-developer <inky@mailbox.org>
2026-09-13 18:43:56 +0200

    Add github pages workflow

--- svelte.config.ts
+++ svelte.config.ts
@@ -3,13 +3,16 @@ import { type Config } from '@sveltejs/kit';
 
 const config: Config = {
 	kit: {
+		paths: {
+			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH as `/${string}`)
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
-			precompress: false,
+			precompress: true,
 			strict: true
 		})
 	}
